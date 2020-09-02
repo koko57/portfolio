b0046a03e127a343b7ba93ca9a4aa4d6fde72dfd
Agata Kosior <agata.kosior57@gmail.com>
2020-09-02 19:36:53 +0200

    Create views for all pages

---
+++ src/views/ContactView.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Title } from '../components/styled/text/title';
+import Navbar from '../components/navbar';
+import { ContactLinksWrapper } from '../components/styled/links/contactLinksWrapper';
+import { ContactLink } from '../components/styled/links/contactLink';
+import { GitHub, Linkedin, Mail } from 'react-feather';
+
+export const ContactView = () => {
+	return (
+		<>
+			<Title>Contact</Title>
+			<ContactLinksWrapper className="links">
+				<ContactLink href="https://github.com/koko57" target="_blank">
+					<span className="logo">
+						<GitHub
+							size={48}
+							color={"#343434"}
+							strokeWidth={"1px"}
+						/>
+					</span>
+					<span className="name">GitHub</span>
+				</ContactLink>
+				<ContactLink
+					href="https://www.linkedin.com/in/agata-kosior/"
+					target="_blank"
+				>
+					<span className="logo">
+						<Linkedin
+							size={48}
+							color={"#343434"}
+							strokeWidth={"1px"}
+						/>
+					</span>
+					<span className="name">LinkedIn</span>
+				</ContactLink>
+				<ContactLink href="mailto:agata.kosior57@gmail.com">
+					<span className="logo">
+						<Mail size={48} color={"#343434"} strokeWidth={"1px"} />
+					</span>
+					<span className="name">Mail</span>
+				</ContactLink>
+			</ContactLinksWrapper>
+			<Navbar />
+		</>
+	);
+};
---
+++ src/views/HomePageView.js
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components";
+import { Text } from "../components/styled/text/text";
+import { Title } from "../components/styled/text/title";
+import Navbar from "../components/navbar";
+import { IconsContainer } from "../containers/IconsContainer";
+
+export const HomePageWrapper = styled.div`
+	padding: 0 4rem;
+
+	@media screen and (max-width: 480px) {
+		padding: 1rem 2.5rem;
+	}
+
+	@media screen and (max-height: 480px) {
+		padding: 0 2rem;
+	}
+`;
+
+export const HomePageView = () => {
+	// todo: move navbar to page wrapper
+	return (
+		<>
+			<HomePageWrapper>
+				<Title main>A.</Title>
+				<Text>
+					<p>Agata Kosior</p>
+					<p>Front-End / JavaScript Developer</p>
+					<p>
+						Programming enthusiast, coffee connoiseur, music
+						aficionado.
+						<br />
+						Aspiring to become a Full Stack Ninja. <br />
+						Looking for new challenges in the IT world.
+					</p>
+				</Text>
+				<IconsContainer />
+			</HomePageWrapper>
+			<Navbar />
+		</>
+	);
+};
